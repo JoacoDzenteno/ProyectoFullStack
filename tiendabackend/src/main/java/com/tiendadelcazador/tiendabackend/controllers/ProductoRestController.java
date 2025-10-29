e1d58b26c7d7219d8bef18a63d59b9dbbf0af8bf
CubitoEstudioso <ig.lopezf@duocuc.cl>
2025-10-29 14:59:44 -0300

    Add files via upload
    Se crean/actualizan el resto de Rest Controllers

--- tiendabackend/src/main/java/com/tiendadelcazador/tiendabackend/controllers/ProductoRestController.java
+++ tiendabackend/src/main/java/com/tiendadelcazador/tiendabackend/controllers/ProductoRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tiendadelcazador.tiendabackend.entities.Producto;
 import com.tiendadelcazador.tiendabackend.services.ProductoService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoRestController {
@@ -61,8 +63,8 @@ public class ProductoRestController {
     }
 
     @PatchMapping("/{id}/desactivar")
-    public ResponseEntity<Producto> desactiveProducto(@PathVariable Long id) {
-        return ResponseEntity.ok(productoService.desactiveProducto(id));
+    public ResponseEntity<Producto> deactiveProducto(@PathVariable Long id) {
+        return ResponseEntity.ok(productoService.deactiveProducto(id));
     }
     
 }
